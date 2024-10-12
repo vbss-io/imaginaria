04c045c81732315fd5eeddaa02cab3fd73c3d24f
Vitor Bastos <vbss-services@outlook.com>
2024-10-11 21:05:58 -0300

    feat: component to show loading for images

---
+++ src/presentation/components/Image/index.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+
+import { Loading } from '@/presentation/components/Loading';
+
+import * as S from './styles';
+
+interface ImageProps {
+  src: string,
+  alt: string
+}
+export const LoadImage = ({ src, alt }: ImageProps) => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setIsLoading(false)
+    }
+    img.src = `${import.meta.env.VITE_CDN}${src}`;
+  }, [src])
+
+  return (
+    <S.Container>
+      {
+        isLoading ? (
+          <Loading />
+        ) : (
+          <img
+            src={`${import.meta.env.VITE_CDN}${src}`}
+            alt={alt}
+            loading="lazy"
+          />
+        )
+      }
+    </S.Container>
+  );
+};
---
+++ src/presentation/components/Image/styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '@/presentation/config/stitches.config';
+
+export const Container = styled('div', {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+});
