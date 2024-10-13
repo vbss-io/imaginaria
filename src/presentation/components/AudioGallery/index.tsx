11bf3bc0a3919c4c90b1115118c964155f5022f7
Vitor Bastos <vbss-services@outlook.com>
2024-10-13 16:43:45 -0300

    feat: add video and audio galleries

---
+++ src/presentation/components/AudioGallery/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+
+export const AudioGallery = () => {
+  return (
+    <S.Container>
+      <S.Text>
+        Áudios
+      </S.Text>
+      <S.Title>
+        Em Breve
+      </S.Title>
+      <S.SubTitle>
+        Volte Logo
+      </S.SubTitle>
+    </S.Container>
+  );
+};
---
+++ src/presentation/components/VideoGallery/index.tsx
@@ -0,0 +1,17 @@
+import * as S from './styles';
+
+export const VideoGallery = () => {
+  return (
+    <S.Container>
+      <S.Text>
+        Vídeos
+      </S.Text>
+      <S.Title>
+        Em Breve
+      </S.Title>
+      <S.SubTitle>
+        Volte Logo
+      </S.SubTitle>
+    </S.Container>
+  );
+};
