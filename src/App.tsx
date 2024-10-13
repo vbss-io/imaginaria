d96ef004bc5bf1849a7506ca491fe2e95b766c67
Vitor Bastos <vbss-services@outlook.com>
2024-10-13 16:44:45 -0300

    feat: move header to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { registerDependencies } from '@/infra/dependency-injection/Register';
 import { Footer } from '@/presentation/components/Footer';
+import { Header } from '@/presentation/components/Header';
 import { Loading } from '@/presentation/components/Loading';
 import { Root } from '@/presentation/config/stitches.config';
 import { DarkModeProvider } from '@/presentation/contexts/dark-mode-context.tsx';
+import { TabProvider } from '@/presentation/contexts/tab-context';
 import { Home } from '@/presentation/pages/Home';
 import { NotFound } from '@/presentation/pages/NotFound';
 
@@ -25,10 +27,13 @@ function App() {
 
   return (
     <DarkModeProvider>
-      <Root>
-        <RouterProvider router={router} fallbackElement={<Loading />}/>
-        <Footer />
-      </Root>
+      <TabProvider>
+        <Root>
+          <Header />
+          <RouterProvider router={router} fallbackElement={<Loading />}/>
+          <Footer />
+        </Root>
+      </TabProvider>
     </DarkModeProvider>
   )
 }
--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,56 +1,32 @@
-
-import { CassetteTape, Image, Video } from '@phosphor-icons/react';
-import { useState } from 'react';
-import { Button } from 'vbss-ui';
-
-import { Header } from '@/presentation/components/Header';
+import { AudioGallery } from '@/presentation/components/AudioGallery';
 import { ImageGallery } from '@/presentation/components/ImageGallery';
+import { VideoGallery } from '@/presentation/components/VideoGallery';
+import { useTab } from '@/presentation/hooks/use-tab';
 
 import * as S from './styles';
 
 export const Home = () => {
-  const [tab, setTab] = useState<Tab>('images')
+  const { tab } = useTab()
 
   return (
     <S.Container>
-      <Header />
-      <S.TabsContainer>
-        <Button
-          variant={tab === 'images' ? 'secondary' : 'primary'}
-          rounded="full"
-          onClick={() => setTab('images')}
-        >
-          <Image color="white" width="1.3rem" height="1.3rem"/>
-          Imagens
-        </Button>
-        <Button
-          variant={tab === 'videos' ? 'secondary' : 'primary'}
-          rounded="full"
-          onClick={() => setTab('videos')}
-          disabled
-        >
-          <Video color="white" width="1.3rem" height="1.3rem"/>
-          Vídeos
-        </Button>
-        <Button
-          variant={tab === 'audios' ? 'secondary' : 'primary'}
-          rounded="full"
-          onClick={() => setTab('audios')}
-          disabled
-        >
-          <CassetteTape color="white" width="1.3rem" height="1.3rem"/>
-          Audios
-        </Button>
-      </S.TabsContainer>
       <S.Content>
         {
           tab === 'images' && (
             <ImageGallery />
           )
         }
+        {
+          tab === 'videos' && (
+            <VideoGallery />
+          )
+        }
+        {
+          tab === 'audios' && (
+            <AudioGallery />
+          )
+        }
       </S.Content>
     </S.Container>
   );
 };
-
-type Tab = 'images' | 'videos' | 'audios'
