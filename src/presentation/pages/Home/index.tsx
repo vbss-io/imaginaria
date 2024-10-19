3e95fc4b055c0a44db7179fef357aeadb76cc684
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:49:02 -0300

    feat: add batch gallery in home page

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,31 +1,21 @@
-import { AudioGallery } from '@/presentation/components/AudioGallery';
-import { ImageGallery } from '@/presentation/components/ImageGallery';
-import { VideoGallery } from '@/presentation/components/VideoGallery';
-import { useTab } from '@/presentation/hooks/use-tab';
+import { AudioGallery } from "@/presentation/components/AudioGallery";
+import { ImageGallery } from "@/presentation/components/ImageGallery";
+import { VideoGallery } from "@/presentation/components/VideoGallery";
+import { useTab } from "@/presentation/hooks/use-tab";
 
-import * as S from './styles';
+import { BatchGallery } from "@/presentation/components/BatchGallery";
+import * as S from "./styles";
 
 export const Home = () => {
-  const { tab } = useTab()
+  const { tab } = useTab();
 
   return (
     <S.Container>
       <S.Content>
-        {
-          tab === 'images' && (
-            <ImageGallery />
-          )
-        }
-        {
-          tab === 'videos' && (
-            <VideoGallery />
-          )
-        }
-        {
-          tab === 'audios' && (
-            <AudioGallery />
-          )
-        }
+        {tab === "batches" && <BatchGallery />}
+        {tab === "images" && <ImageGallery />}
+        {tab === "videos" && <VideoGallery />}
+        {tab === "audios" && <AudioGallery />}
       </S.Content>
     </S.Container>
   );
