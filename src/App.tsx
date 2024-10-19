797a0d256885cb91b212b804282d6ef50601b94a
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:45:10 -0300

    feat: auth context auth prover app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,45 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { registerDependencies } from '@/infra/dependency-injection/Register';
-import { Footer } from '@/presentation/components/Footer';
-import { Header } from '@/presentation/components/Header';
-import { Loading } from '@/presentation/components/Loading';
-import { Root } from '@/presentation/config/stitches.config';
-import { DarkModeProvider } from '@/presentation/contexts/dark-mode-context.tsx';
-import { TabProvider } from '@/presentation/contexts/tab-context';
-import { Home } from '@/presentation/pages/Home';
-import { NotFound } from '@/presentation/pages/NotFound';
-
+import { registerDependencies } from "@/infra/dependency-injection/Register";
+import { Footer } from "@/presentation/components/Footer";
+import { Header } from "@/presentation/components/Header";
+import { Loading } from "@/presentation/components/Loading";
+import { Root } from "@/presentation/config/stitches.config";
+import { DarkModeProvider } from "@/presentation/contexts/dark-mode-context.tsx";
+import { TabProvider } from "@/presentation/contexts/tab-context";
+import { Home } from "@/presentation/pages/Home";
+import { NotFound } from "@/presentation/pages/NotFound";
+import { AuthProvider } from "./presentation/contexts/auth-context";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
-    children: [{
-      path: '/image/:id',
-    }]
+    children: [
+      {
+        path: "/image/:id",
+      },
+    ],
   },
-  { path: '*', element: <NotFound /> },
-])
+  { path: "*", element: <NotFound /> },
+]);
 
 function App() {
-  registerDependencies()
+  registerDependencies();
 
   return (
-    <DarkModeProvider>
-      <TabProvider>
-        <Root>
-          <Header />
-          <RouterProvider router={router} fallbackElement={<Loading />}/>
-          <Footer />
-        </Root>
-      </TabProvider>
-    </DarkModeProvider>
-  )
+    <AuthProvider>
+      <DarkModeProvider>
+        <TabProvider>
+          <Root>
+            <Header />
+            <RouterProvider router={router} fallbackElement={<Loading />} />
+            <Footer />
+          </Root>
+        </TabProvider>
+      </DarkModeProvider>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
