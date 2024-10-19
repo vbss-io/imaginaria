d507309ead5d48cfa5fa0d92343ad4e477113e7a
Vitor Bastos <vbss-services@outlook.com>
2024-10-19 19:46:10 -0300

    feat: update styles for image load

--- src/presentation/components/Image/index.tsx
+++ src/presentation/components/Image/index.tsx
@@ -1,37 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Loading } from '@/presentation/components/Loading';
+import { Loading } from "@/presentation/components/Loading";
 
-import * as S from './styles';
+import * as S from "./styles";
 
 interface ImageProps {
-  src: string,
-  alt: string
+  src: string;
+  alt: string;
 }
 export const LoadImage = ({ src, alt }: ImageProps) => {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const img = new Image();
     img.onload = () => {
-      setIsLoading(false)
-    }
+      setIsLoading(false);
+    };
     img.src = `${import.meta.env.VITE_CDN}${src}`;
-  }, [src])
+  }, [src]);
 
   return (
     <S.Container>
-      {
-        isLoading ? (
-          <Loading />
-        ) : (
-          <img
-            src={`${import.meta.env.VITE_CDN}${src}`}
-            alt={alt}
-            loading="lazy"
-          />
-        )
-      }
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <img
+          src={`${import.meta.env.VITE_CDN}${src}`}
+          alt={alt}
+          loading="lazy"
+        />
+      )}
     </S.Container>
   );
 };
