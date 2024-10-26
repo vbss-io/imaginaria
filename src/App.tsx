b87e010a5679c3b23d930c012f92f50cfdf2342e
Vitor Bastos <vbss-services@outlook.com>
2024-10-26 12:59:52 -0300

    feat: update app pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,41 @@ import { Header } from "@/presentation/components/Header";
 import { Loading } from "@/presentation/components/Loading";
 import { Root } from "@/presentation/config/stitches.config";
 import { DarkModeProvider } from "@/presentation/contexts/dark-mode-context.tsx";
-import { TabProvider } from "@/presentation/contexts/tab-context";
-import { Home } from "@/presentation/pages/Home";
 import { NotFound } from "@/presentation/pages/NotFound";
+import { ImageGallery } from "./presentation/components/ImageGallery";
 import { AuthProvider } from "./presentation/contexts/auth-context";
+import { Audios } from "./presentation/pages/Audios";
+import { Batches } from "./presentation/pages/Batches";
+import { Home } from "./presentation/pages/Home";
+import { Images } from "./presentation/pages/Images";
+import { Videos } from "./presentation/pages/Videos";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/batches",
+    element: <Batches />,
+  },
+  {
+    path: "/images",
+    element: <Images />,
     children: [
       {
-        path: "/image/:id",
+        path: "/images/:id",
+        element: <ImageGallery />,
       },
     ],
+  },
+  {
+    path: "/videos",
+    element: <Videos />,
+  },
+  {
+    path: "/audios",
+    element: <Audios />,
   },
   { path: "*", element: <NotFound /> },
 ]);
@@ -30,13 +51,11 @@ function App() {
   return (
     <AuthProvider>
       <DarkModeProvider>
-        <TabProvider>
-          <Root>
-            <Header />
-            <RouterProvider router={router} fallbackElement={<Loading />} />
-            <Footer />
-          </Root>
-        </TabProvider>
+        <Root>
+          <Header />
+          <RouterProvider router={router} fallbackElement={<Loading />} />
+          <Footer />
+        </Root>
       </DarkModeProvider>
     </AuthProvider>
   );
