5b023f190f86a9a5fd99a109d6bfed2ce17a0116
Vitor Bastos <vbss-services@outlook.com>
2024-10-26 12:58:17 -0300

    feat: add Audio and VIdeo page

---
+++ src/presentation/pages/Audios/index.tsx
@@ -0,0 +1,10 @@
+import { AudioGallery } from "@/presentation/components/AudioGallery";
+
+export const Audios = () => {
+  return (
+    <>
+      <title>Ai Content Factory - Áudios</title>
+      <AudioGallery />
+    </>
+  );
+};
---
+++ src/presentation/pages/Videos/index.tsx
@@ -0,0 +1,10 @@
+import { VideoGallery } from "@/presentation/components/VideoGallery";
+
+export const Videos = () => {
+  return (
+    <>
+      <title>Ai Content Factory - Vídeos</title>
+      <VideoGallery />
+    </>
+  );
+};
