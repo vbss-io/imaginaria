309f00f2bbef7430347bd5e85f7c0bd0d699159c
Vitor Bastos <vbss-services@outlook.com>
2024-10-26 12:58:29 -0300

    feat: home page navigate to images

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,22 +1,12 @@
-import { AudioGallery } from "@/presentation/components/AudioGallery";
-import { ImageGallery } from "@/presentation/components/ImageGallery";
-import { VideoGallery } from "@/presentation/components/VideoGallery";
-import { useTab } from "@/presentation/hooks/use-tab";
-
-import { BatchGallery } from "@/presentation/components/BatchGallery";
-import * as S from "./styles";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
-  const { tab } = useTab();
+  const navigate = useNavigate();
 
-  return (
-    <S.Container>
-      <S.Content>
-        {tab === "batches" && <BatchGallery />}
-        {tab === "images" && <ImageGallery />}
-        {tab === "videos" && <VideoGallery />}
-        {tab === "audios" && <AudioGallery />}
-      </S.Content>
-    </S.Container>
-  );
+  useEffect(() => {
+    navigate("/images");
+  }, [navigate]);
+
+  return <></>;
 };
