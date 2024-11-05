48a3f42573007d5a09e49097aca6cb894ccb1e9d
Vitor Bastos <vbss-services@outlook.com>
2024-11-05 08:44:14 -0300

    feat: add video and video details models

---
+++ src/domain/models/Video/VideoDetails.ts
@@ -0,0 +1,16 @@
+export interface VideoDetails {
+  id: string;
+  width: number;
+  height: number;
+  aspectRatio: string;
+  path: string;
+  createdAt: string;
+  prompt: string;
+  origin: string;
+  modelName: string;
+  isNew: boolean;
+  userLiked: boolean;
+  owner: boolean;
+  authorName: string;
+  automatic: boolean;
+}
