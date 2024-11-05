f38e71be014e069206f29e0d5e4b6b6fb99ba1ed
Vitor Bastos <vbss-services@outlook.com>
2024-11-05 08:45:26 -0300

    feat: add a load video component

---
+++ src/presentation/components/LoadVideo/index.tsx
@@ -0,0 +1,72 @@
+import { Loading } from "@/presentation/components/Loading";
+import { useEffect, useRef, useState } from "react";
+
+import { PlayCircle } from "@phosphor-icons/react";
+import * as S from "./styles";
+
+interface VideoProps {
+  src: string;
+  alt: string;
+  controls?: boolean;
+}
+
+export const LoadVideo = ({ src, alt, controls = false }: VideoProps) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPlayLoading, setIsPlayLoading] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const video = document.createElement("video");
+    video.onloadeddata = () => {
+      setIsLoading(false);
+    };
+    video.src = `${import.meta.env.VITE_CDN}${src}`;
+  }, [src]);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      setIsPlayLoading(true);
+      videoRef.current.play();
+      videoRef.current.onended = () => {
+        videoRef.current!.currentTime = 0;
+        videoRef.current!.play();
+      };
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current && !controls) {
+      videoRef.current.pause();
+      videoRef.current.currentTime = 0;
+      setIsPlayLoading(false);
+    }
+  };
+
+  return (
+    <S.Container>
+      <S.PlayIconWrapper isLoading={isPlayLoading}>
+        <PlayCircle weight="fill" />
+      </S.PlayIconWrapper>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <video
+          ref={videoRef}
+          controls={controls}
+          aria-label={alt}
+          width="100%"
+          height="100%"
+          src={`${import.meta.env.VITE_CDN}${src}`}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onPause={() => {
+            setIsPlayLoading(false);
+          }}
+          onPlay={() => {
+            setIsPlayLoading(true);
+          }}
+        />
+      )}
+    </S.Container>
+  );
+};
---
+++ src/presentation/components/LoadVideo/styles.ts
@@ -0,0 +1,43 @@
+import { keyframes, styled } from "@/presentation/config/stitches.config";
+
+export const Container = styled("div", {
+  position: "relative",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100%",
+});
+
+export const blink = keyframes({
+  "0%, 100%": { opacity: 1 },
+  "50%": { opacity: 0.2 },
+});
+
+export const PlayIconWrapper = styled("div", {
+  position: "absolute",
+  top: 0,
+  left: 0,
+  margin: "0.5rem",
+  padding: "0.5rem",
+  borderRadius: "2rem",
+  backgroundColor: "rgba(0, 0, 0, 0.75)",
+  width: "2rem",
+  height: "2rem",
+
+  svg: {
+    width: "100%",
+    height: "100%",
+    color: "white",
+  },
+
+  variants: {
+    isLoading: {
+      true: {
+        svg: {
+          animation: `${blink} 1.5s infinite ease-in-out`,
+        },
+      },
+    },
+  },
+});
