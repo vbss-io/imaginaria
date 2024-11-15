a3ca5162c1893619ebc9bb99d7ea8ea6775b1692
Vitor Bastos <vbss-services@outlook.com>
2024-11-15 12:58:49 -0300

    feat: update batch gallery with new components

--- src/presentation/components/Batches/BatchGallery/index.tsx
+++ src/presentation/components/Batches/BatchGallery/index.tsx
@@ -1,4 +1,3 @@
-import { Image, Video } from "@phosphor-icons/react";
 import { useEffect, useRef, useState } from "react";
 
 import { Batch as BatchModel } from "@/domain/models/Batch/Batch";
@@ -7,29 +6,39 @@ import { DeleteBatch } from "@/application/usecases/Batch/DeleteBatch";
 import { GetBatchesInput } from "@/application/usecases/Batch/dtos/GetBatches.dto";
 import { GetBatches } from "@/application/usecases/Batch/GetBatches";
 import { BatchFilters } from "@/presentation/components/Batches/BatchFilters";
+import { BatchInformation } from "@/presentation/components/Batches/BatchInformation";
+import { BatchMediaPreview } from "@/presentation/components/Batches/BatchMediaPreview";
 import { Loading } from "@/presentation/components/General/Loading";
-import { ImageDetails } from "@/presentation/components/Images/ImageDetails";
-import { VideoDetails } from "@/presentation/components/Videos/VideoDetails";
-import { useAuth } from "@/presentation/hooks/use-auth";
-import { useNavigate } from "react-router-dom";
+import { useInfiniteScroll } from "@/presentation/hooks/use-infinite-scroll";
 import { Button } from "vbss-ui";
 import * as S from "./styles";
 
 type Batches = BatchModel[];
 
 export const BatchGallery = () => {
-  const { user } = useAuth();
   const [batches, setBatches] = useState<Batches>([]);
   const [filters, setFilters] = useState<Omit<GetBatchesInput, "page">>({});
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const page = useRef<number>(0);
-  const hasMore = useRef(true);
-  const navigate = useNavigate();
+
+  useEffect(() => {
+    const refreshBatches = async () => {
+      page.current = 0;
+      setBatches([]);
+      setFilters({});
+      await getBatches(false);
+    };
+
+    window.addEventListener("refreshMedias", refreshBatches);
+    return () => {
+      window.removeEventListener("refreshMedias", refreshBatches);
+    };
+  }, []);
 
   const getBatches = async (scroll?: boolean) => {
+    const getBatchesUsecase = new GetBatches();
     if (!scroll) setIsLoading(true);
     page.current = page.current + 1;
-    const getBatchesUsecase = new GetBatches();
     const responseBatches = await getBatchesUsecase.execute({
       page: page.current,
       ...filters,
@@ -41,35 +50,22 @@ export const BatchGallery = () => {
     return responseBatches;
   };
 
+  const { medias: scrollBatches } = useInfiniteScroll({
+    getMedias: getBatches,
+  });
+
   useEffect(() => {
     const loadBatches = async () => {
       await getBatches();
     };
-    hasMore.current = true;
     page.current = 0;
     loadBatches();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [filters]);
 
   useEffect(() => {
-    const handleScroll = async () => {
-      const scrollPosition = window.scrollY + window.innerHeight;
-      const pageHeight = document.documentElement.scrollHeight;
-      if (scrollPosition >= pageHeight && hasMore.current) {
-        const newBatches = await getBatches(true);
-        if (!newBatches.length) {
-          hasMore.current = false;
-          return;
-        }
-        setBatches((prev) => [...prev, ...newBatches]);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filters]);
+    if (scrollBatches.length)
+      setBatches((prev) => [...prev, ...(scrollBatches as BatchModel[])]);
+  }, [scrollBatches]);
 
   const handleDeleteBatch = async (id: string) => {
     const deleteBatchUsecase = new DeleteBatch();
@@ -89,147 +85,21 @@ export const BatchGallery = () => {
         {!isLoading && batches.length
           ? batches.map((batch) => (
               <S.BatchContainer key={batch.id}>
-                <S.BatchDetailsHeader>
-                  <S.Status status={batch.status}>{batch.status}</S.Status>
-                  {batch.images.map((image) => (
-                    <S.MediaDialog
-                      key={image.id}
-                      trigger={
-                        <S.MediaContainer
-                          onClick={() =>
-                            navigate(`/batches/images/${image.id}`)
-                          }
-                        >
-                          <S.TypeTag>
-                            <Image fill="white" />
-                          </S.TypeTag>
-                          <img
-                            key={image.id}
-                            src={`${import.meta.env.VITE_CDN}${image.path}`}
-                          />
-                        </S.MediaContainer>
-                      }
-                      title="Imagem"
-                      description="Detalhe da Imagem"
+                <BatchMediaPreview batch={batch} />
+                <BatchInformation batch={batch} />
+                <S.BatchActions>
+                  <S.DeleteDialog
+                    title="Excluir Batch"
+                    description="As imagens também serão excluidas."
+                    trigger={<Button as="div">Excluir</Button>}
+                  >
+                    <Button
+                      onClick={async () => await handleDeleteBatch(batch.id)}
                     >
-                      <ImageDetails id={image.id} backPath="/batches" />
-                    </S.MediaDialog>
-                  ))}
-                  {batch.videos.map((video) => (
-                    <S.MediaDialog
-                      key={video.id}
-                      trigger={
-                        <S.MediaContainer
-                          onClick={() =>
-                            navigate(`/batches/videos/${video.id}`)
-                          }
-                        >
-                          <S.TypeTag>
-                            <Video fill="white" />
-                          </S.TypeTag>
-                          <video
-                            src={`${import.meta.env.VITE_CDN}${video.path}`}
-                          />
-                        </S.MediaContainer>
-                      }
-                      title="Video"
-                      description="Detalhe do Video"
-                    >
-                      <VideoDetails id={video.id} backPath="/batches" />
-                    </S.MediaDialog>
-                  ))}
-                  <S.BatchDetailsHeaderInfo>
-                    <S.BatchDetailsInfoCard>
-                      <strong>Prompt</strong>
-                      <span>{batch.prompt}</span>
-                    </S.BatchDetailsInfoCard>
-                    {batch.negativePrompt !== "none" && (
-                      <S.BatchDetailsInfoCard>
-                        <strong>Negative Prompt:</strong>
-                        <span>{batch.negativePrompt}</span>
-                      </S.BatchDetailsInfoCard>
-                    )}
-                  </S.BatchDetailsHeaderInfo>
-                </S.BatchDetailsHeader>
-                <S.BatchDetailsContent>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Nº Imagens:</strong>
-                    <span>{batch.images.length}</span>
-                  </S.BatchDetailsInfoCard>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Origem:</strong>
-                    <span>{batch.origin}</span>
-                  </S.BatchDetailsInfoCard>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Modelo:</strong>
-                    <span>{batch.modelName}</span>
-                  </S.BatchDetailsInfoCard>
-                  {batch.sampler !== "none" && (
-                    <S.BatchDetailsInfoCard>
-                      <strong>Sampler:</strong>
-                      <span>{batch.sampler}</span>
-                    </S.BatchDetailsInfoCard>
-                  )}
-                  {batch.scheduler !== "none" && (
-                    <S.BatchDetailsInfoCard>
-                      <strong>Scheduler:</strong>
-                      <span>{batch.scheduler}</span>
-                    </S.BatchDetailsInfoCard>
-                  )}
-                  {batch.steps !== 0 && (
-                    <S.BatchDetailsInfoCard>
-                      <strong>Steps:</strong>
-                      <span>{batch.steps}</span>
-                    </S.BatchDetailsInfoCard>
-                  )}
-                  {batch.size !== 0 && (
-                    <S.BatchDetailsInfoCard>
-                      <strong>Size:</strong>
-                      <span>{batch.size}</span>
-                    </S.BatchDetailsInfoCard>
-                  )}
-                  <S.BatchDetailsInfoCard>
-                    <strong>Geração:</strong>
-                    <span>{batch.automatic ? "Automática" : "Manual"}</span>
-                  </S.BatchDetailsInfoCard>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Autor:</strong>
-                    <span>{batch.authorName}</span>
-                  </S.BatchDetailsInfoCard>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Criação:</strong>
-                    <span>
-                      {new Date(batch.createdAt).toLocaleDateString()}
-                    </span>
-                  </S.BatchDetailsInfoCard>
-                  <S.BatchDetailsInfoCard>
-                    <strong>Atualização:</strong>
-                    <span>
-                      {new Date(batch.updatedAt).toLocaleDateString()}
-                    </span>
-                  </S.BatchDetailsInfoCard>
-                  {batch.errorMessage && (
-                    <S.BatchDetailsInfoCard>
-                      <strong>Erro:</strong>
-                      <span>{batch.errorMessage}</span>
-                    </S.BatchDetailsInfoCard>
-                  )}
-                </S.BatchDetailsContent>
-                {(user?.isAdmin || batch.owner) && (
-                  <S.BatchActions>
-                    <S.DeleteDialog
-                      title="Excluir Batch"
-                      description="As imagens também serão excluidas."
-                      trigger={<Button as="div">Excluir</Button>}
-                    >
-                      <Button
-                        onClick={async () => await handleDeleteBatch(batch.id)}
-                      >
-                        Confirmar
-                      </Button>
-                    </S.DeleteDialog>
-                  </S.BatchActions>
-                )}
+                      Confirmar
+                    </Button>
+                  </S.DeleteDialog>
+                </S.BatchActions>
               </S.BatchContainer>
             ))
           : null}
--- src/presentation/components/Batches/BatchGallery/styles.ts
+++ src/presentation/components/Batches/BatchGallery/styles.ts
@@ -41,131 +41,10 @@ export const BatchContainer = styled("div", {
   gap: "1rem",
 });
 
-export const BatchDetailsHeader = styled("div", {
-  position: "relative",
-  width: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-start",
-  gap: "1rem",
-  padding: "1rem",
-  backgroundColor: "$primary",
-  borderRadius: "1rem",
-  flexDirection: "column",
-
-  "@md": {
-    flexDirection: "row",
-  },
-});
-
-export const MediaContainer = styled("div", {
-  position: "relative",
-  display: "flex",
-  gap: "1rem",
-
-  img: {
-    maxWidth: "10rem",
-    height: "auto",
-    maxHeight: "10rem",
-    width: "auto",
-    borderRadius: "1rem",
-  },
-
-  video: {
-    maxWidth: "10rem",
-    height: "auto",
-    maxHeight: "10rem",
-    width: "auto",
-    borderRadius: "1rem",
-  },
-
-  "@md": {
-    flexDirection: "row",
-  },
-});
-
-export const TypeTag = styled("div", {
-  position: "absolute",
-  top: 0,
-  left: 0,
-  height: "1.25rem",
-  width: "1.25rem",
-  margin: "0.5rem",
-
-  svg: {
-    width: "100%",
-    height: "100%",
-  },
-});
-
-export const BatchDetailsHeaderInfo = styled("div", {
-  display: "flex",
-  flexDirection: "column",
-  color: "white",
-  gap: "1rem",
-  fontSize: "0.85rem",
-  lineHeight: "0.85rem",
-});
-
-export const BatchDetailsInfoCard = styled("div", {
-  display: "flex",
-  alignItems: "flex-start",
-  flexDirection: "column",
-  gap: "0.5rem",
-
-  strong: {
-    opacity: 0.5,
-  },
-});
-
-export const BatchDetailsContent = styled("div", {
-  display: "flex",
-  flexWrap: "wrap",
-  padding: "1rem",
-  gap: "2rem",
-  color: "$background",
-
-  strong: {
-    fontWeight: 500,
-  },
-
-  span: {
-    fontWeight: 600,
-  },
-});
 export const BatchActions = styled("div", {
   display: "flex",
   gap: "1rem",
   alignSelf: "flex-end",
-});
-
-export const Status = styled("div", {
-  position: "absolute",
-  display: "flex",
-  top: 0,
-  right: 0,
-  fontWeight: 700,
-  fontSize: "0.75rem",
-  padding: "0.2rem 1rem",
-  borderRadius: "0 1rem 0 1rem",
-  color: "white",
-
-  variants: {
-    status: {
-      queued: {
-        backgroundColor: "gray",
-      },
-      processing: {
-        backgroundColor: "blue",
-      },
-      processed: {
-        backgroundColor: "$secondary",
-      },
-      error: {
-        backgroundColor: "red",
-      },
-    },
-  },
 });
 
 export const DeleteDialog = styled(Dialog, {
@@ -187,19 +66,4 @@ export const NoData = styled("div", {
   lineHeight: "1.5rem",
   fontWeight: "bold",
   color: "$primary",
-});
-
-export const MediaDialog = styled(Dialog, {
-  backgroundColor: "$background",
-  color: "$text",
-  maxHeight: "95vh",
-  maxWidth: "90vw",
-
-  h2: {
-    color: "$background",
-  },
-
-  p: {
-    display: "none",
-  },
 });
