93e44e5e73a1bca220dcb906a2dde00d7292460c
Vitor Bastos <vbss-services@outlook.com>
2024-11-16 19:47:09 -0300

    feat: add constants for tasks options

---
+++ src/domain/constants/taskConsts.ts
@@ -0,0 +1,12 @@
+export const cronTimeValues = [
+  { label: "1 segundo", value: "*/1 * * * * *" },
+  { label: "30 segundos", value: "*/30 * * * * *" },
+  { label: "12 horas", value: "0 0 */12 * * *" },
+  { label: "24 horas", value: "0 0 */24 * * *" },
+];
+
+export const originValues = [
+  { label: "Midjourney (GoAPI)", value: "goApiMidjourney" },
+  { label: "Dall-E 3", value: "openAiDalle3" },
+  { label: "Luma Labs", value: "LumaLabs" },
+];
