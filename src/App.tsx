b3d6008aa0dc55b4a66e31b4a4901ebb7aa9d06d
Vitor Bastos <vbss-services@outlook.com>
2025-06-04 20:21:19 -0300

    refactor: update new application entries

--- src/App.tsx
+++ src/App.tsx
@@ -1,131 +1,34 @@
-import { registerDependencies } from "@/infra/dependency-injection/Register";
-import { BatchGallery } from "@/presentation/components/Batches/BatchGallery";
-import { Footer } from "@/presentation/components/General/Footer";
-import { Loading } from "@/presentation/components/General/Loading";
-import { Header } from "@/presentation/components/Header";
-import { ImageDetails } from "@/presentation/components/Images/ImageDetails";
-import { ImageGallery } from "@/presentation/components/Images/ImageGallery";
-import { TaskGallery } from "@/presentation/components/Tasks/TasksGallery";
-import { VideoDetails } from "@/presentation/components/Videos/VideoDetails";
-import { VideoGallery } from "@/presentation/components/Videos/VideoGallery";
-import { Root } from "@/presentation/config/stitches.config";
-import { AuthProvider } from "@/presentation/contexts/auth-context";
-import { DarkModeProvider } from "@/presentation/contexts/dark-mode-context.tsx";
-import { Audios } from "@/presentation/pages/Audios";
-import { Home } from "@/presentation/pages/Home";
-import { Images } from "@/presentation/pages/Images";
-import { Login } from "@/presentation/pages/Login";
-import { NotFound } from "@/presentation/pages/NotFound";
-import { Profile } from "@/presentation/pages/Profile";
-import { Videos } from "@/presentation/pages/Videos";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/images",
-    element: <Images />,
-    children: [
-      {
-        path: "/images/:id",
-        element: <ImageDetails />,
-      },
-    ],
-  },
-  {
-    path: "/image/:id",
-    element: <ImageDetails />,
-  },
-  {
-    path: "/videos",
-    element: <Videos />,
-    children: [
-      {
-        path: "/videos/:id",
-        element: <VideoDetails />,
-      },
-    ],
-  },
-  {
-    path: "/video/:id",
-    element: <VideoDetails />,
-  },
-  {
-    path: "/audios",
-    element: <Audios />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/signin",
-    element: <Login />,
-  },
-  {
-    path: "/profile",
-    element: <Profile />,
-    children: [
-      {
-        path: "/profile/batches",
-        element: <BatchGallery />,
-        children: [
-          {
-            path: "/profile/batches/images/:id",
-            element: <ImageDetails />,
-          },
-          {
-            path: "/profile/batches/videos/:id",
-            element: <VideoDetails />,
-          },
-        ],
-      },
-      {
-        path: "/profile/tasks",
-        element: <TaskGallery />,
-      },
-      {
-        path: "/profile/images",
-        element: <ImageGallery callUserImages />,
-        children: [
-          {
-            path: "/profile/images/:id",
-            element: <ImageDetails />,
-          },
-        ],
-      },
-      {
-        path: "/profile/videos",
-        element: <VideoGallery callUserVideos />,
-        children: [
-          {
-            path: "/profile/videos/:id",
-            element: <VideoDetails />,
-          },
-        ],
-      },
-    ],
-  },
-  { path: "*", element: <NotFound /> },
-]);
+import { registerDependencies } from '@/infra/dependency-injection/register'
+import { Footer } from '@/presentation/components/general/footer'
+import { Header } from '@/presentation/components/general/header'
+import { Loading } from '@/presentation/components/general/loading'
+import { AuthProvider } from '@/presentation/providers/auth-provider'
+import { DarkModeProvider } from '@/presentation/providers/dark-mode-provider'
+import { router } from '@/presentation/router'
+import { styled } from '@stitches/react'
+
+export const AppContainer = styled('div', {
+  minHeight: 'calc(100vh - 4.5rem)',
+  display: 'flex',
+  flexDirection: 'column'
+})
 
 function App() {
-  registerDependencies();
+  registerDependencies()
 
   return (
-    <AuthProvider>
-      <DarkModeProvider>
-        <Root>
-          <Header />
+    <DarkModeProvider>
+      <AuthProvider>
+        <Header />
+        <AppContainer>
           <RouterProvider router={router} fallbackElement={<Loading />} />
-          <Footer />
-        </Root>
-      </DarkModeProvider>
-    </AuthProvider>
-  );
+        </AppContainer>
+        <Footer />
+      </AuthProvider>
+    </DarkModeProvider>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "vbss-ui/dist/style.css"
+import './index.css'
 
-import App from '@/App.tsx'
-import "@/customColors.css"
-import { registerDependencies } from '@/infra/dependency-injection/Register'
-import { globalCss } from '@/presentation/config/stitches.config'
-
-registerDependencies()
-
-const globalStyles = globalCss({
-  html: {
-    backgroundColor: "$background",
-  },
-
-  body: {
-    unset: 'all',
-    margin: 0,
-    padding: 0,
-    border: 0,
-    fontFamily: "$default",
-
-    '::-webkit-scrollbar': {
-      width: '0.25rem'
-    },
-  
-    '::-webkit-scrollbar-track': {
-      backgroundColor: '#E5E7EB',
-      borderRadius: '1rem'
-    },
-  
-    '::-webkit-scrollbar-thumb': {
-      backgroundColor: '$primary',
-      borderRadius: '1rem',
-
-      '&:hover': {
-        backgroundColor: '$highlight',
-      },
-
-      '&:active': {
-        backgroundColor: '$highlight',
-      },
-    },
-  }
-});
+import App from '@/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {globalStyles()}
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
