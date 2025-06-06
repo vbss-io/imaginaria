0651cb077bb923bf8dd5ee4a4ba1a83437000d2b
Vitor Bastos <vbss-services@outlook.com>
2025-06-06 10:53:47 -0300

    chore: temp general components and pages

--- src/presentation/pages/images/styles.ts
+++
@@ -1,8 +0,0 @@
-import { styled } from '@/presentation/styles/theme'
-
-export const Container = styled('div', {
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  gap: '1.5rem'
-})
